972d0e36c567b04d06722d5c5328eb69d4cccc5e
Seba Kerckhof <seba.kerckhof@gmail.com>
2016-04-11 19:59:25 +0200

    update dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'seba:minifiers-autoprefixer',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'Standard minifiers used with Meteor apps by default + css autoprefixer.',
   git: 'https://github.com/sebakerckhof/meteor-minifiers-autoprefix.git',
   documentation: 'README.md'
@@ -9,12 +9,12 @@ Package.describe({
 Package.registerBuildPlugin({
   name: "minifyStdCSS",
   use: [
-    'minifier-css'
+    'minifier-css@1.1.10'
   ],
   npmDependencies: {
     "source-map": "0.5.3",
-    "postcss": "5.0.14",
-    "autoprefixer": "6.3.1"
+    "postcss": "5.0.19",
+    "autoprefixer": "6.3.6"
   },
   sources: [
     'plugin/minify-css.js'
